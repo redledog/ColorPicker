0861a7b0469bb485ea272f92488ee009bff8e870
redledog <godelder1585@gmail.com>
2020-08-19 00:30:36 +0900

    HTML 용 컬러값 표현해주는 칸 생성 LineEdit 위젯 클릭시 자동으로 클립보드에 복사해줌

--- src/Screens/main_screen.py
+++ src/Screens/main_screen.py
@@ -1,12 +1,16 @@
 import os
 
+from PyQt5 import QtCore
 from PyQt5.QtGui import QIcon, QColor
-from PyQt5.QtWidgets import QAction, qApp, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QColorDialog, QFrame
+from PyQt5.QtWidgets import QAction, qApp, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QColorDialog, QFrame, QLineEdit
 from src.Screens.screen import Screen
 from src.Informations import Information as Info
-
+import clipboard
 
 class MainScreen(Screen):
+    colordialog = None
+    colorBox = None
+    html_lbl = None
 
     def init(self):
         self.setWindowTitle('Color Picker!')
@@ -18,7 +22,6 @@ class MainScreen(Screen):
         self.setWindowIcon(
             QIcon(os.path.join(Info.IMG_PATH, 'screen-icon.png'))
         )
-
 
     def makeMenubar(self):
         exitAction = QAction('Exit', self)
@@ -35,25 +38,44 @@ class MainScreen(Screen):
 
     def makeCentralContent(self):
         wg = QWidget()
-        col = QColor(0, 0, 0)
+        col = QColor(255, 255, 255)
 
-        colordialog = QColorDialog()
-        colorBox = QFrame()
+        self.colordialog = QColorDialog()
+        self.colorBox = QFrame()
 
-        colordialog.setOption(QColorDialog.NoButtons)
-        colordialog.setOption(QColorDialog.ShowAlphaChannel)
+        self.html_lbl = Label('HTML : ')
+
+        linedit = QLineEdit()
+        linedit.setReadOnly(True)
+        linedit.setText(col.name())
+        linedit.mousePressEvent = lambda ev: clipboard.copy(linedit.text())
+
+        # self.colordialog.currentColorChanged.connect(self.on_changed_color)
+        self.colordialog.currentColorChanged.connect(lambda: self.on_changed_color(self.colordialog, self.colorBox, linedit))
+
+
+        self.colordialog.setOption(QColorDialog.NoButtons)
+        self.colordialog.setOption(QColorDialog.ShowAlphaChannel)
         #colordialog.setOption(QColorDialog.DontUseNativeDialog, False)
 
-        colorBox.setStyleSheet('QWidget { background-color: %s }' % col.name())
-        colorBox.setGeometry(0, 0, 100, 100)
+        self.colorBox.setStyleSheet('QWidget { background-color: %s }' % col.name())
+        self.colorBox.setGeometry(0, 0, 100, 100)
 
         hbox = QHBoxLayout()
-        hbox.addWidget(colordialog)
+        hbox.addWidget(self.colordialog)
+
+        hbox_html = QHBoxLayout()
+        hbox_html.addWidget(self.html_lbl)
+
+        hbox_html.addWidget(linedit, 1)
+
 
         vbox_colorbox = QVBoxLayout()
         #vbox_colorbox.addStretch(1)
-        vbox_colorbox.addWidget(colorBox, 1)
-        vbox_colorbox.addStretch(3)
+        vbox_colorbox.addWidget(self.colorBox, 1)
+        # vbox_colorbox.addStretch(1)
+        vbox_colorbox.addLayout(hbox_html, 1)
+        vbox_colorbox.addStretch(2)
 
         hbox.addLayout(vbox_colorbox)
 
@@ -62,9 +84,36 @@ class MainScreen(Screen):
         vbox.addLayout(hbox)
         vbox.addStretch(1)
 
-        Info.MAIN_SCREEN_WIDTH += colordialog.width()
-        Info.MAIN_SCREEN_WIDTH += colorBox.width()
+        Info.MAIN_SCREEN_WIDTH += self.colordialog.width()
+        Info.MAIN_SCREEN_WIDTH += self.colorBox.width()
 
         wg.setLayout(vbox)
         return wg
 
+    def on_changed_color(self, dialog, frame, line_edit):
+        color = dialog.currentColor()
+        frame.setStyleSheet('QWidget { background-color: %s }' % color.name())
+        # print(color.name())
+        line_edit.setText(color.name())
+
+
+class Label(QLabel):
+    header_str = None
+    body_str = None
+
+    def __init__(self, header):
+        super().__init__(header)
+        # self.focusInEvent(self.on_click_label)
+        self.header_str = header
+        self.text = self.header_str
+
+    def set_body(self, body):
+        self.body_str = body
+        self.text = self.header_str + self.body_str
+
+    def on_click_label(self):
+        print(self.body_str)
+
+
+class LineEdit(QLineEdit):
+    pass
